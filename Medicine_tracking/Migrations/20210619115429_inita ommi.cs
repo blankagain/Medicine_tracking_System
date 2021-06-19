2dd0a476fbb866c75e44b88f70aa75da0626dce9
Akshay Kumar <akshay.chinki@gmail.com>
2021-06-19 17:24:41 +0530

    Intial commit migration

---
+++ Medicine_tracking/Migrations/20210619115429_inita ommi.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Medicine_tracking.Migrations
+{
+    public partial class initaommi : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MediDB",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "varchar(500)", unicode: false, maxLength: 500, nullable: false),
+                    Brand = table.Column<string>(type: "varchar(500)", unicode: false, maxLength: 500, nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<int>(type: "int", nullable: false),
+                    Expiry_date = table.Column<int>(type: "int", nullable: false),
+                    Notes = table.Column<string>(type: "varchar(500)", unicode: false, maxLength: 500, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MediDB", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MediDB");
+        }
+    }
+}
